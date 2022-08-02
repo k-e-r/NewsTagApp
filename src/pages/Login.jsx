b90e7b12c63b5db458e42d58a1892a9177393951
k-e-r <damajonboua@gmail.com>
2022-08-01 20:49:00 -0700

    modify login/register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
-import { useState, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
 
-import { authActions } from '../store/auth-slice';
-import classes from './Login.module.css';
+import { authActions } from "../store/auth-slice";
+import classes from "./Login.module.css";
+import { ReactComponent as EyeIcon } from "../assets/eye.svg";
+import { ReactComponent as EyeOffIcon } from "../assets/eye-off.svg";
 
-const apikey = process.env['REACT_APP_AUTH_APIKEY'];
+const apikey = process.env["REACT_APP_AUTH_APIKEY"];
 
 const calculateRemainingTime = (expirationTime) => {
   const currentTime = new Date().getTime();
@@ -22,9 +24,14 @@ const Login = () => {
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
+  };
+
+  const clickHandler = () => {
+    setVisible((prev) => !prev);
   };
 
   const submitHandler = (event) => {
@@ -41,14 +48,14 @@ const Login = () => {
       url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apikey}`;
     }
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         email: enteredEmail,
         password: enteredPassword,
         returnSecureToken: true,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then((res) => {
@@ -57,7 +64,7 @@ const Login = () => {
           return res.json();
         } else {
           return res.json().then((data) => {
-            let errorMessage = 'Authentication failed!';
+            let errorMessage = "Authentication failed!";
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
@@ -90,24 +97,40 @@ const Login = () => {
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
+          <label className={classes.label} htmlFor='email'>
+            Email Address
+          </label>
           <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
+          <label className={classes.label} htmlFor='password'>
+            Password
+          </label>
           <input
-            type='password'
+            type={!visible ? "password" : "text"}
             id='password'
             required
             ref={passwordInputRef}
           />
+          {!visible && (
+            <EyeOffIcon
+              className={classes.eyeIcon}
+              onClick={() => clickHandler()}
+            />
+          )}
+          {visible && (
+            <EyeIcon
+              className={classes.eyeIcon}
+              onClick={() => clickHandler()}
+            />
+          )}
         </div>
         <div className={classes.actions}>
           {!isLoading && (
-            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+            <button>{isLogin ? "Login" : "Create Account"}</button>
           )}
           {isLoading && <p>Sending request...</p>}
           <button
@@ -115,7 +138,7 @@ const Login = () => {
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
