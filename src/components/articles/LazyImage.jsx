1dd0c694a679ce27927f5a466afd068395c924b6
k-e-r <damajonboua@gmail.com>
2022-08-01 21:00:53 -0700

    modify loading image size

--- src/components/articles/LazyImage.jsx
+++ src/components/articles/LazyImage.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-import classes from './Articles.module.css';
-import notImage from '../../assets/image-not-found-scaled.jpg';
+import classes from "./Articles.module.css";
+import notImage from "../../assets/image-not-found-scaled.jpg";
 
 const LazyImage = ({ article }) => {
   const [loading, setLoading] = useState(false);
@@ -15,12 +15,12 @@ const LazyImage = ({ article }) => {
   };
 
   const imageErrorHandler = (url) => {
-    document.getElementById('img--' + url).src = `${notImage}`;
+    document.getElementById("img--" + url).src = `${notImage}`;
     if (!loading) setLoading(true);
   };
 
   useEffect(() => {
-    if (document.getElementById('img--' + article.image)?.complete) {
+    if (document.getElementById("img--" + article.image)?.complete) {
       if (!loading) setLoading(true);
     }
   }, [article]);
@@ -30,7 +30,7 @@ const LazyImage = ({ article }) => {
       <div className={classes.imageContainer}>
         {article.image ? (
           <>
-            {!loading && <Skeleton />}
+            {!loading && <Skeleton height={520} width={1000} />}
             <img
               onLoad={() => imageLoadedHandler(article.image)}
               onError={() => imageErrorHandler(article.image)}
